298f812da743e214d34aa2b2eb1b97a63ac8bcc8
Anna Mika <aniamika90@gmail.com>
2020-11-26 23:22:22 +0100

    lab2 - part1

---
+++ Wsei.ExchangeThings.Web/Controllers/ApiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Wsei.ExchangeThings.Web.Models;
+
+namespace Wsei.ExchangeThings.Web.Controllers
+{
+    [ApiController]
+    [Route("api/company")]
+    public class ApiController : ControllerBase
+    {
+        public IActionResult Post(CompanyModel company)
+        {
+            try
+            {
+                CompanyAddedViewModel response = new CompanyAddedViewModel
+                {
+                    NumberOfCharsInName = company.Name.Length,
+                    NumberOfCharsInDescription = company.Description.Length,
+                    IsHidden = !company.IsVisible
+                };
+
+                return Ok(response);
+            }
+            catch (Exception exception)
+            {
+                return NotFound(exception);
+            }
+        }
+ 
+    }
+}
