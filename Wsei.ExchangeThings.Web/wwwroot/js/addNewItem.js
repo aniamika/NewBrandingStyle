25e75c816e083c3fdc3edf8c06cd6383b316bbe3
Anna Mika <aniamika90@gmail.com>
2020-11-29 11:51:58 +0100

    lab2 - part2

--- Wsei.ExchangeThings.Web/wwwroot/js/addNewItem.js
+++ Wsei.ExchangeThings.Web/wwwroot/js/addNewItem.js
@@ -3,12 +3,26 @@
     const formElement = document.forms[0];
     const addNewItem = async () => {
         // 1. read data from the form
-        // const requestData = ...
+        const url = "/api/company"
+        const requestData = JSON.stringify({
+            Name: formElement.querySelector('input[name="Name"]').value,
+            Description: formElement.querySelector('input[name="Description"]').value,
+            isVisible: formElement.querySelector('input[name="IsVisible"]').checked
+        });
+        
+        const jsonConfig = {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: requestData,
+        };
         // 2. call the application server using fetch method
-        // const response = await fetch(...);
+        const response = await fetch(url, jsonConfig);
         const responseJson = await response.json();
-        if (responseJson.success) {
-            // 3. un-hide the alertElement when the request has been successful
+
+        if (response.status === 200 && responseJson) {
+            // 3 un-hide the alertElement when the request has been successful
             alertElement.style.display = "block";
         }
     };
